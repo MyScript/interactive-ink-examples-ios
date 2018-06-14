b90c8a160979d2e8261d564d52a045e97ea0e041
MyScript <packages@myscript.com>
2018-06-14 11:49:46 +0200

    Update standard recognition assets retrieval script to download 1.2 assets

--- Examples/GetStartedSwift/retrieve_recognition-assets.sh
+++ Examples/GetStartedSwift/retrieve_recognition-assets.sh
@@ -12,20 +12,24 @@ fi
 
 ProjectRootFolder=$1
 
-if [ ! -d "$ProjectRootFolder/Resources/recognition-assets" ] || [ ! -d "$ProjectRootFolder/Resources/recognition-assets/conf" ] || [ ! -f "$ProjectRootFolder/Resources/recognition-assets/conf/diagram.conf" ] || [ ! -f "$ProjectRootFolder/Resources/recognition-assets/conf/en_US.conf" ] || [ ! -f "$ProjectRootFolder/Resources/recognition-assets/conf/math.conf" ] || [ ! -d "$ProjectRootFolder/Resources/recognition-assets/resources" ] || [ ! -d "$ProjectRootFolder/Resources/recognition-assets/resources/analyzer" ] || [ ! -d "$ProjectRootFolder/Resources/recognition-assets/resources/en_US" ] || [ ! -d "$ProjectRootFolder/Resources/recognition-assets/resources/math" ] || [ ! -d "$ProjectRootFolder/Resources/recognition-assets/resources/shape" ]; then
+if [ ! -f "$ProjectRootFolder/Resources/recognition-assets/conf/diagram.conf" ] || [ ! -f "$ProjectRootFolder/Resources/recognition-assets/conf/raw-content.conf" ] || [ ! -f "$ProjectRootFolder/Resources/recognition-assets/conf/math.conf" ] || [ ! -f "$ProjectRootFolder/Resources/recognition-assets/conf/en_US.conf" ]; then
 
 echo MyScript en-US recognition assets retriever
 
-curl -O https://s3-us-west-2.amazonaws.com/iink/assets/1.1.0/myscript-iink-recognition-diagram.zip
+curl -O https://s3-us-west-2.amazonaws.com/iink/assets/1.2.0/myscript-iink-recognition-diagram.zip
 unzip -o myscript-iink-recognition-diagram.zip -d $ProjectRootFolder/Resources/
 
-curl -O https://s3-us-west-2.amazonaws.com/iink/assets/1.1.0/myscript-iink-recognition-math.zip
+curl -O https://s3-us-west-2.amazonaws.com/iink/assets/1.2.0/myscript-iink-recognition-raw-content.zip
+unzip -o myscript-iink-recognition-raw-content.zip -d $ProjectRootFolder/Resources/
+
+curl -O https://s3-us-west-2.amazonaws.com/iink/assets/1.2.0/myscript-iink-recognition-math.zip
 unzip -o myscript-iink-recognition-math.zip -d $ProjectRootFolder/Resources/
 
-curl -O https://s3-us-west-2.amazonaws.com/iink/assets/1.1.0/myscript-iink-recognition-text-en_US.zip
+curl -O https://s3-us-west-2.amazonaws.com/iink/assets/1.2.0/myscript-iink-recognition-text-en_US.zip
 unzip -o myscript-iink-recognition-text-en_US.zip -d $ProjectRootFolder/Resources/
 
 rm myscript-iink-recognition-diagram.zip
+rm myscript-iink-recognition-raw-content.zip
 rm myscript-iink-recognition-math.zip
 rm myscript-iink-recognition-text-en_US.zip
 
